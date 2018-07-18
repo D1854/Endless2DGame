1c08a6cc76bcf51209d76b9561d1bae4eabe8684
XTITANPROELITE9\Daniel <D00160870@student.dkit.ie>
2018-07-18 17:39:55 +0100

    First Setup, Added player script and Challenge Script

---
+++ Endless Side Scroller/Assets/Scripts/ChallengeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeManager : MonoBehaviour {
+
+	public float SCROLL_SPEED = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//scrolling
+
+		GameObject currentChild; // (BAD CODE - THIS SHOULD BE DEFINED EARLIER)
+
+		//Loops through all the children of the object
+		for (int i = 0; i < transform.childCount; i++) {
+
+			//Get child at current index
+			currentChild = transform.GetChild (i).gameObject;
+
+			//Sends the object to be scrolled
+			ScrollChallenge (currentChild);
+
+		}
+
+
+	}
+
+
+
+	void ScrollChallenge (GameObject currentChallenege)
+	{
+
+		currentChallenge.transform.position -= Vector3.right * (SCROLL_SPEED * Time.deltaTime);
+
+
+	}
+
+}
---
+++ Endless Side Scroller/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float JUMP_POWER = 10.0f;
+
+	Rigidbody2D myBody;
+	public bool grounded = false;
+
+	// Use this for initialization
+	void Start () {
+		myBody = transform.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// if user presses space and we are grounded, then jump
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+
+			//jump
+			myBody.AddForce (Vector3.up * (JUMP_POWER * myBody.mass * myBody.gravityScale * 20.0f));
+
+		}
+		
+	}
+
+	//called when first collides, sets grounded
+	void OnCollisionEnter2D (Collision2D other){
+
+		//if the tag of what we just collided with is "Ground"
+		if (other.collider.tag == "Ground") {
+			grounded = true;
+		}
+
+	}
+
+	//called during continious collision, sets grounded to false
+	void OnCollisionStay2D (Collision2D other){
+
+		//if the tag of what we just collided with is "Ground"
+		if (other.collider.tag == "Ground") {
+			grounded = true;
+		}
+
+	}
+
+	//called when collisison ends, sets grounded to false
+	void OnCollisionExit2D (Collision2D other){
+
+		//if the tag of what we just collided with is "Ground"
+		if (other.collider.tag == "Ground") {
+			grounded = false;
+		}
+
+	}
+}
